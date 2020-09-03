84ccd2218feb1783e346c6d3d4d8c6cdda764108
David Schaller <schaller.david@yahoo.de>
2020-09-03 15:11:15 +0200

    simplifies oe_failure

--- examples/oe_failure.py
+++ examples/oe_failure.py
@@ -13,13 +13,13 @@ from dockin.oe_docking import get_structure_from_pdb, select_chain, select_altlo
     create_hybrid_receptor, hybrid_docking
 
 
-def redocking(jobs, log_file, jobs_number):
+def redocking(jobs):
     """
     Redock co-crystalized ligands to their protein and redirect any warning or error to the specified log file.
 
     Parameters
     ----------
-    jobs: list of pd.Series
+    jobs: multiprocessing.manager.list of pd.Series
         List of pandas series containing the information to run the docking.
 
     log_file: str
@@ -29,21 +29,15 @@ def redocking(jobs, log_file, jobs_number):
         Total number of jobs to run
     """
 
-    # redirect OpenEye logging
-    ofs = oechem.oeofstream()
-    ofs.open(log_file)
-    oechem.OEThrow.SetOutputStream(ofs)
-
     while jobs:
         # run docking jobs until jobs list is empty
         try:
             job = jobs.pop(0)
         except IndexError:
-            pass
+            break
         # perform docking and catch any error
         try:
             pdb_id = job['pdb']
-            oechem.OEThrow.Info(f'Starting DocKin for {pdb_id}')
             structure_complex = get_structure_from_pdb(pdb_id)
             structure_complex = select_chain(structure_complex, job['chain'])
             altloc = job['alt']
@@ -56,20 +50,14 @@ def redocking(jobs, log_file, jobs_number):
             hybrid_receptor = create_hybrid_receptor(protein, ligand, CWD / f'data/{pdb_id}_hybrid_receptor.oeb')
             docking_poses = hybrid_docking(hybrid_receptor, [ligand],
                                            docking_poses_save_path=CWD / f'data/{pdb_id}_hybrid_re-docking.sdf')
-            rmsd = rmsds_from_sdfs(str(CWD / f'data/{pdb_id}_{ligand_id}.sdf'),
-                                   str(CWD / f'data/{pdb_id}_hybrid_re-docking.sdf'))[0]
-            oechem.OEThrow.Info(f'Redocking RMSD for {pdb_id}: {rmsd}')
-            print(f'Redocking: Finished {jobs_number - len(jobs)}/{jobs_number}')
         except:  # not PEP8, but want to catch any error
-            oechem.OEThrow.Info('DocKin error!')
             continue
     return
 
 
 if __name__ == "__main__":
     # parse arguments
-    parser = argparse.ArgumentParser(prog='DocKin', description='Redock all ligands to kinases',
-                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(prog='DocKin', description='Redock all ligands to kinases')
     parser.add_argument('-p', dest='num_processes', help='number of parallel processes', type=int, default=1)
     num_processes = int(parser.parse_args().num_processes)
 
@@ -109,9 +97,7 @@ if __name__ == "__main__":
         jobs.append(row)
 
     # starting jobs on separate processes
-    processes = [multiprocessing.Process(target=redocking,
-                                         args=(jobs, str(CWD / f'data/oe_failure_{process_counter}.log'),
-                                               len(jobs))) for process_counter in range(num_processes)]
+    processes = [multiprocessing.Process(target=redocking, args=(jobs,)) for process_counter in range(num_processes)]
     for process in processes:
         process.start()
     for process in processes:
